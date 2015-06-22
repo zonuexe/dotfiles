3b97512c7dcbdb5036566edc25bd199f360ef593
USAMI Kenta <tadsan@zonu.me>
2015-06-23 00:09:40 +0900

    Update (Add multiple-cursors)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -610,6 +610,31 @@
     (bind-key "<wheel-right>" 'image-forward-hscroll  image-mode-map)
     (bind-key "<wheel-left>"  'image-backward-hscroll image-mode-map)))
 
+;; multiple-cursors
+;; http://qiita.com/ongaeshi/items/3521b814aa4bf162181d
+(use-package multiple-cursors
+  :init
+  (progn
+    (require 'smartrep)
+    (declare-function smartrep-define-key "smartrep")
+    (bind-key "C-M-c" 'mc/edit-lines)
+    (bind-key "C-M-r" 'mc/mark-all-in-region)
+    (global-unset-key (kbd "C-t"))
+    (smartrep-define-key global-map "C-t"
+      '(("C-t" . 'mc/mark-next-like-this)
+        ("n"   . 'mc/mark-next-like-this)
+        ("p"   . 'mc/mark-previous-like-this)
+        ("m"   . 'mc/mark-more-like-this-extended)
+        ("u"   . 'mc/unmark-next-like-this)
+        ("U"   . 'mc/unmark-previous-like-this)
+        ("s"   . 'mc/skip-to-next-like-this)
+        ("S"   . 'mc/skip-to-previous-like-this)
+        ("*"   . 'mc/mark-all-like-this)
+        ("d"   . 'mc/mark-all-like-this-dwim)
+        ("i"   . 'mc/insert-numbers)
+        ("o"   . 'mc/sort-regions)
+        ("O"   . 'mc/reverse-regions)))))
+
 ;; smooth-scroll https://github.com/k-talo/smooth-scroll.el
 (use-package smooth-scroll
   :init
