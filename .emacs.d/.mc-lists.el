ecb3523387597bb2938320c1da0dc5fb4013d553
USAMI Kenta <tadsan@zonu.me>
2016-09-07 09:55:21 +0900

    Update

--- .emacs.d/.mc-lists.el
+++ .emacs.d/.mc-lists.el
@@ -27,6 +27,7 @@
         org-end-of-line
         org-force-self-insert
         org-self-insert-command
+        orgtbl-hijacker-command-102
         orgtbl-hijacker-command-109
         orgtbl-self-insert-command
         paredit-close-round
