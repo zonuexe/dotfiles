8691361854d68895a42fe3706dd29c3bf702c61c
USAMI Kenta <tadsan@zonu.me>
2017-01-09 00:12:37 +0900

    Update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -151,7 +151,7 @@
   (bind-key  "M-ESC ESC"   'keyboard-quit)
   (bind-key  "C-S-n"       'make-frame)
   (bind-key  "C-S-w"       'delete-frame)
-  (bind-key  "M-N"         'my/new-untitled-buffer)
+  (bind-key  "M-N"         'untitled-new-buffer)
   (bind-key  "C-c :"       'right-click-context-menu)
   (bind-key  "C-c ;"       'imenu)
   (bind-key  "C-c R"       'revert-buffer)
@@ -1172,11 +1172,6 @@ http://ergoemacs.org/emacs/elisp_datetime.html"
                      (setq buffer-file-coding-system 'binary)
                      (buffer-substring-no-properties (point-min) (point-max)))))
       (f-write-bytes content to-file))))
-
-(defun my/new-untitled-buffer ()
-  "Create and switch to untitled buffer."
-  (interactive)
-  (switch-to-buffer (generate-new-buffer "Untitled")))
 
 (defun my/term-mode-hook ()
   ""
