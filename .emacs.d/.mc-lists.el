41fe6cb553eb87665f70f0b46a41bb1d6851de49
USAMI Kenta <tadsan@zonu.me>
2017-05-10 13:26:58 +0900

    Update

--- .emacs.d/.mc-lists.el
+++ .emacs.d/.mc-lists.el
@@ -12,6 +12,7 @@
         c-electric-semi&comma
         c-electric-slash
         c-electric-star
+        coffee-dedent-line-backspace
         delete-indentation
         electric-newline-and-maybe-indent
         end-of-buffer
