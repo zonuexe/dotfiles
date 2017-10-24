523fe47d305a87e3ac960251551eead00d74cc20
USAMI Kenta <tadsan@zonu.me>
2017-10-24 15:00:12 +0900

    Update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -342,6 +342,7 @@
   (--each '("\\.html?\\'" "\\.tpl\\'" "\\.tpl\\.xhtml\\'" "\\.ejs\\'" "\\.hbs\\'" "\\(\\.html\\)?\\.erb\\'")
     (add-to-list 'auto-mode-alist (cons it 'web-mode)))
   :config
+  (require 'smartparens)
   (add-to-list 'web-mode-ac-sources-alist
                '("html" . (ac-source-html-tag ac-source-html-attr ac-source-html-attrv)))
   (sp-local-pair 'web-mode "<" nil :when '(sp-web-mode-is-code-context)))
