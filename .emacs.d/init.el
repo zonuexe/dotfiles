370afa3f6255a4bc6cbf6489ae102df09d785015
USAMI Kenta <tadsan@zonu.me>
2018-03-16 10:21:48 +0900

    Update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -127,7 +127,7 @@
 ;; PATH
 (custom-set-variables
  '(exec-path-from-shell-check-startup-files nil)
- '(exec-path-from-shell-variables '("PATH" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "MANPATH" "GOROOT" "GOPATH" "PERL5LIB")))
+ '(exec-path-from-shell-variables '("PATH" "TEST_SERVER" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "MANPATH" "GOROOT" "GOPATH" "PERL5LIB")))
 (exec-path-from-shell-initialize)
 
 ;;; Coding:
