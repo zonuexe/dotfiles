0663cac685fbd2dcead7f1bfebdb915a576efbb1
USAMI Kenta <tadsan@zonu.me>
2021-06-14 04:33:45 +0900

    Add completions-common-part face

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -51,6 +51,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(completions-common-part ((t (:background "black" :foreground "WhiteSmoke" :slant normal :weight normal :height 1.0 :width normal))))
  '(font-lock-doc-face ((t (:slant normal)))))
 
 (defvar my/load-themes '(manoj-dark tango))
